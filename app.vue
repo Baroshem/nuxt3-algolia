f2ec6f33147fb81608782f1bf5036ef8e304b6e7
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-06 12:36:52 +0100

    feat: add @nuxt-commerce/algolia

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    {{ result }}
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+const { result, search } = useSearch('test_index') // pass your index as param
+
+onMounted(async () => {
+  await search({ query: 'Samsung' });
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: [
+    ['@nuxt-commerce/algolia', {
+      apiKey: '<YOUR_SEARCH_API_KEY>',
+      applicationId: '<YOUR_APPLICATION_ID>'
+    }]
+  ],
 })
